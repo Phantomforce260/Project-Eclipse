4a07869932f1cf0c9677c23ad3fdc254da9d43bf
Harrison DiAmbrosio <hdiambrosio@gmail.com>
2026-02-21 15:35:55 -0500

    pizza done, sfx next

---
+++ Assets/Scripts/Minigames/Pizza/PizzaSelectable.cs
@@ -0,0 +1,33 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PizzaSelectable : MonoBehaviour
+{
+    public RectTransform rectTransform;
+    public Image selection;
+    public Sprite listSprite;
+
+    public GameObject topping;
+
+    void Start()
+    {
+        Restart();
+    }
+
+    public void Restart()
+    {
+        topping.SetActive(false);
+        selection.enabled = false;
+    }
+
+    public void SetSelection(bool status)
+    {
+        selection.enabled = status;
+    }
+
+    public void SetTopping(bool status)
+    {
+        topping.SetActive(status);
+        topping.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+    }
+}
