df15f1d50cea85b5a41f98884e8ef37ba877811a
Goose1206 <mermaidgirl1000@gmail.com>
2026-02-21 22:24:37 -0500

    Sort Added

---
+++ Assets/Scripts/Minigames/Sort/Sort.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public enum TrueSealColor
+{
+    Purple,
+    Yellow
+}
+public class Sort : Minigame
+{
+    public Slide slide;
+
+    // visible seals
+    public Image[] slots;
+
+    // seal types
+    public Sprite purpSeal;
+    public Sprite yellSeal;
+
+    // make list of seals
+    public List<TrueSealColor> queue = new List<TrueSealColor>();
+    public int numSeals = 20;
+    int count = 0;
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        TrueSealColor newSeal;
+        // generate seals for seal list 
+        for (int i = 0; i < numSeals; i++)
+        {
+            newSeal = (Random.value > 0.5f) ? TrueSealColor.Purple : TrueSealColor.Yellow;
+            queue.Add(newSeal);
+        }
+        Rerender();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // handle input
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            if (queue[0] == TrueSealColor.Purple)
+                slide.SlideLeft(purpSeal);
+            else    
+                slide.SlideLeft(yellSeal);
+            Yoink(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            if (queue[0] == TrueSealColor.Purple)
+                slide.SlideRight(purpSeal);
+            else    
+                slide.SlideRight(yellSeal);
+            Yoink(false);
+        }
+    }
+
+    // remove seal
+    public void Yoink(bool wentLeft)
+    {
+        TrueSealColor seal = queue[0];
+        queue.RemoveAt(0);
+        if ((wentLeft && seal == TrueSealColor.Purple) || (!wentLeft && seal == TrueSealColor.Yellow)) // if wrong
+            count++;
+        else     
+            queue.Add(seal);
+        
+        if (count == numSeals)
+            Finish();
+
+        Rerender();
+    }
+
+    // update render on seal removal
+    void Rerender()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < queue.Count)
+            {
+                // Enable image
+                slots[i].color = Color.white;
+
+                if (queue[i] == TrueSealColor.Purple)
+                    slots[i].sprite = purpSeal;
+                else
+                    slots[i].sprite = yellSeal;
+            }
+            else
+            {
+                // Show blank
+                slots[i].sprite = null;
+                slots[i].color = new Color(1, 1, 1, 0); // fully transparent
+            }
+        }
+    }
+
+    void AnimateSlide()
+    {
+        
+    }
+}
